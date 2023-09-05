d716c756dbd800ac1ed15276afac1fba3ca6127e
Hironogawa <hello@hironogawa.com>
2023-09-05 16:38:05 +0200

    Simple fetch exercise  with random user api

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,14 @@
 <script setup>
-import HelloWorld from "./components/OptionAPI/HelloWorld.vue";
-// import HelloWorld from "./components/CompositionAPI/HelloWorld.vue";
 
-import GetCat from "./components/OptionAPI/RandomCatImage.vue";
+import RandomUser from "./components/OptionAPI/RandomUser.vue";
 
-import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="This is passed to to the component, you did it!" />
-      <GetCat />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <RandomUser />
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/OptionAPI/RandomUser.vue
@@ -0,0 +1,75 @@
+<script>
+export default {
+  data() {
+    return {
+      userImg: "",
+      userName: "",
+    };
+  },
+  methods: {
+    async fetchUser() {
+      const response = await fetch("https://randomuser.me/api/");
+      const data = await response.json();
+
+      this.userImg = data.results[0].picture.large;
+      this.userName = data.results[0].name.first;
+    },
+  },
+};
+</script>
+
+<template>
+  <div  class="user-card">
+    <div class="user-img">
+      <img v-if="userImg" :src="userImg" />
+    </div>
+    <h3 class="user-name">{{ userName }}</h3>
+    <button @click="fetchUser" class="user-btn">get new user</button>
+  </div>
+</template>
+<style scoped>
+.user-card{
+  box-sizing: content-box ;
+  height: 200px;
+  width: 150px;
+
+  padding: 24px 12px 12px 12px;
+  
+  border-radius: 12px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  gap: 21px;
+
+  background-color: #eee;
+
+
+}
+.user-img {
+  width: 80px;
+  height: 80px;
+
+  overflow: hidden;
+  border-radius: 100%;
+
+  background-color: grey;
+}
+.user-img img {
+  width: 100%;
+  height: 100%;
+}
+.user-btn{
+  width: 100%;
+  
+  padding: 9px;
+  
+  border-radius: 6px;
+  border: none;
+
+  cursor: pointer;
+  background-color: aquamarine;
+}
+</style>
